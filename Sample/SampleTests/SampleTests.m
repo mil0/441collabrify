d116454e5c0d0d4f87f8a1cf32c4bedc491173e6
milo <mkock@umich.edu>
2013-09-06 16:03:59 -0400

    Created XCode Project

---
+++ Sample/SampleTests/SampleTests.m
@@ -0,0 +1,32 @@
+//
+//  SampleTests.m
+//  SampleTests
+//
+//  Created by milo kock on 9/6/13.
+//  Copyright (c) 2013 441. All rights reserved.
+//
+
+#import "SampleTests.h"
+
+@implementation SampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SampleTests");
+}
+
+@end
